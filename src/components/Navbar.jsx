c84bde805ce40a78b9eb7d4471ad58aa393f2520
Krishna75305 <yadavkrishna75305@gmail.com>
2024-07-07 18:24:39 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ const Navbar = () => {
   return (
     <div className="my-4 flex h-[60px] items-center justify-center gap-2 rounded-lg bg-white text-xl font-medium">
       {/* <img src="/firebase.svg" /> */}
-      <img src="./public\firebase.svg" />
+      {/* <img src="./public\firebase.svg" /> */}
+      <img src="src\components\firebase.svg" alt="" />
       
       <h1>Firebase Contact App</h1>
     </div>
